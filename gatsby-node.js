f6d53beb3daf637b16e02c47982ed47d93c0a21b
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-08-15 20:47:19 +0100

    ao_feat: graphql for pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,37 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require("path");
+const { slash } = require("gatsby-core-utils");
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  //query to get all blog posts
+
+  const {
+    data: {
+      allWpPost: { nodes: allPosts },
+    },
+  } = await graphql(`
+    query {
+      allWpPost {
+        nodes {
+          slug
+        }
+      }
+    }
+  `);
+
+  //   const BlogPost = path.resolve(
+  //     "./src/templates/blog/blogposts/single/single-post.jsx"
+  //   );
+
+  allPosts.forEach(node => {
+    console.log(node);
+    // createPage({
+    //   path: `blog/${node.slug}`,
+    //   component: BlogPost,
+    //   context: {
+    //     slug: node.slug,
+    //   },
+    // });
+  });
+};
