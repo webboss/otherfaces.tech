21d03f3068664545cf42fc1e1ea7b12cc084b628
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-10-03 01:57:42 +0100

    ao_feat: waitlist header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,21 @@
 import * as React from "react";
 
 import Layout from "components/layout";
+import ctl from "@netlify/classnames-template-literals";
 
 const IndexPage = () => (
-  <Layout title="Other Faces of Tech" ignoreSiteName={true}></Layout>
+  <Layout title="Other Faces of Tech" ignoreSiteName={true}>
+    <header className={headerStyle}>
+      <h1>We are cooking something for non-coding techies</h1>
+    </header>
+  </Layout>
 );
 
+const headerStyle = ctl(`
+pt-[256px]
+text-center
+mx-auto
+text-[72px]
+max-w-[847px]
+`);
 export default IndexPage;
