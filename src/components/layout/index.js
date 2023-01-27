57e4d3f6fb9608ff4e34f2f03a66897e481bdae5
Aremu Smog <aremuoluwagbamila@gmail.com>
2023-01-27 04:43:36 +0100

    feat: add background color gradient

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types";
 import Seo from "./seo";
 import { MainFooter } from "components/layout/footer";
 import { NavBar } from "./navbar";
+import ctl from "@netlify/classnames-template-literals";
 
 const Layout = ({ children, title, ignoreSiteName = false }) => {
   return (
@@ -18,9 +19,20 @@ const Layout = ({ children, title, ignoreSiteName = false }) => {
       <NavBar />
       <main className="relative">{children}</main>
       <MainFooter />
+
+      <div
+        className={`radial-gradient-blue  top-[50%] right-0 ${gradientItemStyle}`}
+      />
+      <div
+        className={`radial-gradient-red  top-[20%] left-0 ${gradientItemStyle}`}
+      />
     </>
   );
 };
+
+const gradientItemStyle = ctl(`
+w-[500px] h-[500px] rounded-full opacity-20 fixed z-[-1] pointer-events-none
+`);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
