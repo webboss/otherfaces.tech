11abeea69ebcd5d7abffb9d751b806c8b2f6b4e5
Aremu Smog <aremuoluwagbamila@gmail.com>
2023-08-09 20:18:36 +0100

    ao_feat: update copy

--- src/templates/home/header.jsx
+++ src/templates/home/header.jsx
@@ -7,15 +7,10 @@ export const HomeHeader = () => {
   return (
     <header className="text-primary-100 text-center min-h-screen md:mt-0 mt-8 flex-col flex items-center justify-center">
       <Container>
-        <Text variant="h1" weight="500">
-          Everyone in tech <Br on="mobile" />
-          <span className="line-through">writes code</span>
-        </Text>
-
-        <div className="md:max-w-[950px] max-w-[700px] mx-auto  ">
-          <Text variant="p18" className="mt-[20px] mb-20 max-w-[1000px]">
-            We are telling stories and providing roadmaps to non-coding careers
-            in tech.
+        <div className="md:max-w-[1200px] max-w-[700px] mx-auto  ">
+          <Text variant="h1" className="mb-20" weight="500">
+            Stories, Resources, & Roadmaps <Br on="desktop" />
+            for non-coding techies.
           </Text>
         </div>
         <Button size="large" to="#recent-stories">
